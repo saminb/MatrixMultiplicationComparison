c642d4540134af0856fdaf600496c7eea920cbb5
saminb <63878531+saminb@users.noreply.github.com>
2022-02-15 17:20:39 -0800

    hotfix100

--- MatrixMultiplication_ProgrammingProject/src/Main.java
+++ MatrixMultiplication_ProgrammingProject/src/Main.java
@@ -1,14 +1,22 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.DoubleSummaryStatistics;
+import java.util.List;
 import java.util.Random;
 
 public class Main {
-
-	public static void main(String[] args) {
-		testTiming();
+	public static final String DATA_FILE = "output.csv";
+	public static List<Double> iterativeTimes= new ArrayList();
+	public static List<Double> DivideConquerTimes= new ArrayList();
+	public static List<Double> StrassenTimes= new ArrayList();
+	
+	public static void main(String[] args){
+		runner();
 	}
 	
-	private static void testTiming() {
-		long totalTime, startTime,finishTime = 0;
+	private static void runner() {
 		int [] n= {4,8,16,32,64,128,256,512,1024};
 		
 		for( int i=0; i<n.length;i++){
@@ -16,35 +24,56 @@ public class Main {
 			int[][] matrixB= new int[n[i]][n[i]];
 			matrixA= generateNumbers(matrixA);
 			matrixB= generateNumbers(matrixB);
-			for( int test= 0; test<3; test++) {
-				System.out.println("Iterative");
-				Date startDate = new Date();
-				startTime= startDate.getTime();
-				IterativeMatrixMultiplication.multiply(matrixA,matrixB);
-				Date finishDate = new Date();
-                finishTime = finishDate.getTime();
-                totalTime = (finishTime - startTime);
-                System.out.println("Size of the Matrix:"+n[i]+"\nTime: "+ totalTime+ "ms.");
-                
-                System.out.println("\n Divide N Conquer");
-                Date startDate2 = new Date();
-				startTime= startDate2.getTime();
-				DivideNConquerMatrixMultiplication.multiply(matrixA,matrixB);
-				Date finishDate2 = new Date();
-                finishTime = finishDate2.getTime();
-                totalTime = (finishTime - startTime);
-                System.out.println("Size of the Matrix:"+n[i]+"\nTime: "+ totalTime+ "ms.");
-                
-                System.out.println("\n Strassen");
-                Date startDate3 = new Date();
-				startTime= startDate3.getTime();
-				StrassenMatrixMultiplication.multiply(matrixA,matrixB);
-				Date finishDate3 = new Date();
-                finishTime = finishDate3.getTime();
-                totalTime = (finishTime - startTime);
-                System.out.println("Size of the Matrix:"+n[i]+"\nTime: "+ totalTime+ "ms.");
-			}
+			test(matrixA,matrixB,3);
 		}
+	}
+
+	private static void test(int[][] matrixA, int[][] matrixB, int count) {
+		long totalTime, startTime,finishTime = 0;
+		int size= matrixA.length;
+		System.out.println("Size of Matrix:"+size);
+		for(int i=0; i<count; i++) {
+			Date startDate = new Date();
+			startTime= startDate.getTime();
+			IterativeMatrixMultiplication.multiply(matrixA,matrixB);
+			Date finishDate = new Date();
+            finishTime = finishDate.getTime();
+            totalTime = (finishTime - startTime);
+            iterativeTimes.add((double) totalTime);
+		}
+		System.out.println("Iterative");
+		averageTime(iterativeTimes);
+		for(int i=0; i<count; i++) {
+
+         Date startDate2 = new Date();
+			startTime= startDate2.getTime();
+			DivideNConquerMatrixMultiplication.multiply(matrixA,matrixB);
+			Date finishDate2 = new Date();
+         finishTime = finishDate2.getTime();
+         totalTime = (finishTime - startTime);
+         DivideConquerTimes.add((double) totalTime);
+		}
+		System.out.println("Divide N Conquer");
+		averageTime(DivideConquerTimes);
+		for(int i=0; i<count; i++) {
+         Date startDate3 = new Date();
+			startTime= startDate3.getTime();
+			StrassenMatrixMultiplication.multiply(matrixA,matrixB);
+			Date finishDate3 = new Date();
+         finishTime = finishDate3.getTime();
+         totalTime = (finishTime - startTime);
+         StrassenTimes.add((double) totalTime);
+		}
+		System.out.println("Strassen");
+		averageTime(StrassenTimes);
+	}
+
+	private static void averageTime(List<Double> timeList) {
+		DoubleSummaryStatistics summaryStats = timeList.stream()
+			      .mapToDouble((a) -> a)
+			      .summaryStatistics();
+			      System.out.println(summaryStats.getAverage());
+		
 	}
 
 	private static int[][] generateNumbers(int[][] matrix) {
